ad7bd431a4e08735209345e0c0d3e1c8396a1c37
Ren-David Dimen <rendaviddimen@gmail.com>
2018-04-10 21:53:06 -0400

    update DataParser.java

--- RoadRater/src/roadRater/DataParser.java
+++ RoadRater/src/roadRater/DataParser.java
@@ -107,6 +107,90 @@ public class DataParser {
         }
 	}
 	
+	
+	/**
+	 * parseDataSet
+	 * Reads a string to read a ".txt" file to get the following data
+	 * rdName:	Road Name
+	 * rdType:	Road Type
+	 * rdCity:	Road City
+	 * rdProv:	Province the road is in
+	 * @return rd: Road[] - Returns the populated array of Road objects
+	 * @throws IOException
+	 */
+	public static Road[] parseDataSet(String testData) throws IOException {
+		
+		BufferedReader br = null;
+		Road[] rd = new Road[2];
+		String rdName = "";
+		String rdType = "";
+		String rdCity = "";
+		String rdProvince = "";
+		String currentString = "";
+		int counter = 0;
+		boolean uniqueName = true;
+
+        try {
+            br = new BufferedReader(new FileReader(testData));
+            
+            while ((currentString = br.readLine()) != null) {
+            		
+            		if (currentString.equals("<gml:featureMember>") || !currentString.equals("</gml:featureMember>")) {
+            			if (currentString.contains("<fme:NAME>")) {
+            				rdName = currentString;
+            				rdName = rdName.replace("<fme:NAME>", "");
+            				rdName = rdName.replace("</fme:NAME>", "");
+            			} else if (currentString.contains("<fme:TYPE>")) {
+            				rdType = currentString;
+            				rdType = rdType.replace("<fme:TYPE>", "");
+            				rdType = rdType.replace("</fme:TYPE>", "");
+            			} else if (currentString.contains("<fme:CSDNAME_L>")) {
+            				rdCity = currentString;
+            				rdCity = rdCity.replace("<fme:CSDNAME_L>", "");
+            				rdCity = rdCity.replace("</fme:CSDNAME_L>", "");
+            			} else if (currentString.contains("<fme:PRNAME_L>")) {
+            				rdProvince = currentString;
+            				rdProvince = rdProvince.replace("<fme:PRNAME_L>", "");
+            				rdProvince = rdProvince.replace("</fme:PRNAME_L>", "");
+            			}
+            		} else if (currentString.equals("</gml:featureMember>")) {
+            			if(!rdName.equals("") && !rdType.equals("")) {
+            				
+            				if(counter != 0) {
+	            				for(int i = 0; i < counter; i++) {
+		            				if(rd[i].getRdName().equals(rdName)) {
+		            					uniqueName = false;
+		            					break;
+		            				} else {
+		            					uniqueName = true;
+		            				}
+	            				}
+            				}
+            				
+            				if (uniqueName == true) {
+	            				// Instantiate road object every iteration with
+	                    		Road roadObj = new Road(rdCity, rdProvince, rdName, rdType);
+	                    		rd[counter]= roadObj;
+	                			counter++;
+            				}
+            			}
+            			
+            			rdName = "";
+            			rdType = "";
+            			rdCity = "";
+            			rdProvince = "";
+            		}
+            }
+            System.out.println("Total Roads:\t" + counter + "\n");
+            return rd;
+            
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+        }
+	}
+	
 	/**
 	 * getRoute
 	 * Sends a request to the Google Maps API and receives a JSON response that is then parsed.
